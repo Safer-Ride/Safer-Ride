7bdb33c16cbd3c2babd794ae760d47f1d7e45d2a
rnik22 <80130605+rnik22@users.noreply.github.com>
2021-10-24 20:24:27 -0400

    Update DriverNode.java

--- DriverNode.java
+++ DriverNode.java
@@ -1,29 +1,50 @@
+import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class DriverNode {
 	DummyGPSLocationVar DriverCurrentLocation;
 	int taxiCapacity;
 	int currentCapacity;
-	boolean isIdle;
-	PriorityQueue Assignments;
+	boolean driverState;
+	PriorityQueue <UserNode> Assignments;
 	
-	
-	public DriverNode(DummyGPSLocationVar location, int maxCap, int currCap, boolean status, PriorityQueue schedule) {
+	public DriverNode(DummyGPSLocationVar location, int maxCap, int currCap, boolean status, PriorityQueue <UserNode>schedule) {
 		this.DriverCurrentLocation = location;
 		this.taxiCapacity = maxCap;
 		this.currentCapacity = currCap;
-		this.isIdle = status;
+		this.driverState = status;
 		this.Assignments = schedule;
 	}
 	
-	//Driver Commands 
-		//NextAssignment
-		//PreviousAssignment
-		//Generate newAssingmentQueue 
-		//StartAssignment
-		//EndAssigment
+	public DummyGPSLocationVar getDriverCurrentLocation() {
+		return DriverCurrentLocation;
+	}
+	public void setDriverCurrentLocation(DummyGPSLocationVar driverCurrentLocation) {
+		DriverCurrentLocation = driverCurrentLocation;
+	}
+	public int getTaxiCapacity() {
+		return taxiCapacity;
+	}
+	public void setTaxiCapacity(int taxiCapacity) {
+		this.taxiCapacity = taxiCapacity;
+	}
+	public int getCurrentCapacity() {
+		return currentCapacity;
+	}
+	public void setCurrentCapacity(int currentCapacity) {
+		this.currentCapacity = currentCapacity;
+	}
+	public boolean driverState() {
+		return driverState;
+	}
+	public void setDriverState(boolean driverState) {
+		this.driverState = driverState;
+	}
+	public PriorityQueue getAssignments() {
+		return Assignments;
+	}
+	public void setAssignments(PriorityQueue assignments) {
+		Assignments = assignments;
+	}
 	
-	//Driver
-	
-
 }
