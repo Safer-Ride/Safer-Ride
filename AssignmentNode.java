f2127f6bf236f6dadbc2d4b910705a31f7f8e11b
rnik22 <80130605+rnik22@users.noreply.github.com>
2021-10-24 20:26:45 -0400

    Add files via upload

---
+++ AssignmentNode.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+
+public class AssignmentNode {
+	DriverNode assignedDriver;
+	UserNode assignedUser;
+	ArrayList <UserNode> assignedUsers;
+	
+	public AssignmentNode(DriverNode driver, UserNode user) {
+		this.assignedDriver = driver;
+		this.assignedUser = user;
+		//this.assignedUsers = users;
+	}
+
+	public DriverNode getAssignedDriver() {
+		return assignedDriver;
+	}
+
+	public void setAssignedDriver(DriverNode assignedDriver) {
+		this.assignedDriver = assignedDriver;
+	}
+
+	public UserNode getAssignedUser() {
+		return assignedUser;
+	}
+
+	public void setAssignedUser(UserNode assignedUser) {
+		this.assignedUser = assignedUser;
+	}
+
+	public ArrayList<UserNode> getAssignedUsers() {
+		return assignedUsers;
+	}
+
+	public void setAssignedUsers(ArrayList<UserNode> assignedUsers) {
+		this.assignedUsers = assignedUsers;
+	}
+	
+	
+	
+	
+
+}
