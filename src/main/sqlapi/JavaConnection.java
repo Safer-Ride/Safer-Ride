35b5fc32cc58f398fffc19b1670d388b0fcc05ca
ConaxFox <47067110+ConaxFox@users.noreply.github.com>
2021-10-25 16:04:14 -0400

    Added SqlServer driver connection string and error handling skeleton

--- src/main/sqlapi/JavaConnection.java
+++ src/main/sqlapi/JavaConnection.java
@@ -6,11 +6,15 @@ public class JavaConnection
   {
     try
     {
-
+      Connection sqlCon = DriverManager.getConnection("jdbc:sqlserver://localhost:1433/SaferRideSQL" , "sa" , "asdfghjkl2");
+      Statement statement = sqlCon.createStatement();
+      ResultSet res = statement.executeQuery("select * from Geolocation");
+      
+      sqlCon.close();
     }
-    catch(Exception e)
+    catch(Exception ex)
     {
-
+      System.out.println(ex);
     }
   }
 }
