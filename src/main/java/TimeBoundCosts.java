c8fff55ca9ddaa554463a7ad15cd05170d790ff3
Marco Cabre <mcabre2400@gmail.com>
2021-10-24 21:06:55 -0400

    Create Proper Project Structure

---
+++ src/main/java/TimeBoundCosts.java
@@ -0,0 +1,26 @@
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+
+public class TimeBoundCosts {
+
+     public static void updateWaitTimeCost(UserNode user) {
+         final ScheduledExecutorService scheduler =
+                    Executors.newScheduledThreadPool(1);
+
+         final Runnable waitCost = new Runnable() {
+             public void run() {
+                 //time cost is updated
+                 user.setTimeCost(user.getTimeCost()-5);
+
+             }
+         };
+         final ScheduledFuture<?> waitTimeHandle =
+                 scheduler.scheduleAtFixedRate(waitCost, 150, 150, SECONDS);
+         if (user.isAccepted) {
+             waitTimeHandle.cancel(true);
+         }
+     }
+ }
