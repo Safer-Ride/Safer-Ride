af34d54c8a8f97d7a430f533001396c123995666
Marco Cabre <mcabre2400@gmail.com>
2021-10-25 02:03:05 -0400

    Create initial JUnit Tests for Algorithm

---
+++ src/test/java/DriverNodeTest.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DriverNodeTest {
+
+    @Test
+    void getDriverCurrentLocation() {
+    }
+
+    @Test
+    void setDriverCurrentLocation() {
+    }
+
+    @Test
+    void setCurrentCapacity() {
+    }
+
+    @Test
+    void setDriverState() {
+    }
+
+    @Test
+    void setAssignments() {
+    }
+}
---
+++ src/test/java/UserNodeTest.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserNodeTest {
+
+    @org.junit.jupiter.api.Test
+    void distanceFromDriverCost() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void userCost() {
+    }
+}
