f1fd201a2d7905dead70109cf9869e8de98cc501
Albert L <albertli2019@outlook.com>
2021-11-16 22:02:57 -0500

    fixing api

--- src/main/java/API.java
+++ src/main/java/API.java
@@ -19,25 +19,36 @@ public class API {
 		catch (Exception ex) {
 			System.out.println("Couldn't get API");
 		} */
-		//getMinDuration(47.606209, -122.332069, 47.676891, -122.205986);
+		getMinDuration(47.606209, -122.332069, 47.676891, -122.205986);
 	}
 
 	public static int getMinDuration(double lat1, double long1, double lat2, double long2) {
-		URL url = new URL("https://maps.googleapis.com/maps/api/distancematrix/json?origins=" + lat1 + "%2C" + long1 + "&destinations=" + lat2 + "%2C" + long2 + "&key=AIzaSyDpQAp0FfH4cDRtiXNlwutqJsLuuCAuQa4");
-		HttpURLConnection con = (HttpURLConnection) url.openConnection();
-		con.setRequestMethod("GET");
-		InputStream inputStream = con.getInputStream();
-		BufferedReader br = new BufferedReader(new InputStreamReader((con.getInputStream())));
-		StringBuilder sb = new StringBuilder();
-		String output;
-		while ((output = br.readLine()) != null) {
-			sb.append(output);
+		try {
+			URL url = new URL("https://maps.googleapis.com/maps/api/distancematrix/json?origins=" + lat1 + "%2C" + long1 + "&destinations=" + lat2 + "%2C" + long2 + "&key=AIzaSyDpQAp0FfH4cDRtiXNlwutqJsLuuCAuQa4");
+			System.out.println(url.toString());
+			HttpURLConnection con = (HttpURLConnection) url.openConnection();
+			con.setRequestMethod("GET");
+			// handle error response code it occurs
+		    int responseCode = con.getResponseCode();
+		    InputStream inputStream;
+		    /*if (200 <= responseCode && responseCode <= 299) {
+		        inputStream = con.getInputStream();
+		    } else {
+		        inputStream = con.getErrorStream();
+		    }*/
+		    inputStream = con.getErrorStream();
+			//InputStream inputStream = con.getInputStream();
+			BufferedReader br = new BufferedReader(new InputStreamReader((con.getInputStream())));
+			StringBuilder sb = new StringBuilder();
+			String output;
+			while ((output = br.readLine()) != null) {
+				sb.append(output);
+			}
+			System.out.println(sb.toString());
 		}
-		String[] arr = sb.toString().split("mins\",                  \"value\" : ");
-		String s = new String("arr[1]");
-		Matcher matcher = Pattern.compile("\\d+").matcher(s);
-		matcher.find();
-		int i = Integer.valueOf(matcher.group());
-		System.out.println(i);
+		catch (Exception ex) {
+			System.out.println("Couldn't access API correctly");
+		}
+		return -1;
 	}
 }
